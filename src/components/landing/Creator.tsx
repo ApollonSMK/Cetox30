fbcff17f0b47a1314985a17dee80e212b0a1e2e0
Pablo Silva <microgramxx@gmail.com>
2025-09-04 11:33:02 +0000

    Aqui nesse div ja te disse, ele esta um pouco estranho, acho que poderia

--- src/components/landing/Creator.tsx
+++ src/components/landing/Creator.tsx
@@ -10,14 +10,6 @@ export function Creator() {
             <p className="text-2xl md:text-3xl font-bold font-headline mt-2">Então o CETOX30 foi feito especialmente para SI!</p>
         </div>
         <div className="grid gap-10 lg:grid-cols-2 lg:gap-16 items-center">
-            <Image
-                src="https://picsum.photos/600/600"
-                alt="Criador do Plano Cetox30"
-                width={600}
-                height={600}
-                data-ai-hint="man smiling portrait"
-                className="rounded-xl shadow-2xl mx-auto aspect-square overflow-hidden object-cover"
-            />
             <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl font-headline">Olá, quero mostrar-lhe como consegui <span className="text-primary">perder mais de 20 quilos em apenas 2 meses</span> e, mais importante, como qualquer pessoa também pode perder 10 quilos ou mais.</h2>
@@ -32,6 +24,14 @@ export function Creator() {
                 <a href="#comprar">QUERO TRANSFORMAR O MEU CORPO</a>
                 </Button>
             </div>
+            <Image
+                src="https://picsum.photos/600/600"
+                alt="Criador do Plano Cetox30"
+                width={600}
+                height={600}
+                data-ai-hint="man smiling portrait"
+                className="rounded-xl shadow-2xl mx-auto aspect-square overflow-hidden object-cover"
+            />
         </div>
       </div>
     </section>

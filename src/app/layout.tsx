d2cd6f0aa381dabefc52af08217495a6ca81a13f
ApollonSMK <microgramxx@gmail.com>
2025-09-17 08:47:51 +0000

    mais ele ta adicionado e mesmo assim eu nao recebo nada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Suspense } from 'react';
 import Script from 'next/script';
-import { FB_PIXEL_ID } from '@/lib/fpixel';
 
 export const metadata: Metadata = {
   title: 'Plano Cetox30 - Perca peso em 30 dias',
@@ -22,15 +21,15 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
-        {FB_PIXEL_ID && (
+      <body>
+        {process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID && (
           <Suspense fallback={null}>
             <noscript>
               <img
                 height="1"
                 width="1"
                 style={{ display: 'none' }}
-                src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+                src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
               />
             </noscript>
             <Script
@@ -46,7 +45,7 @@ export default function RootLayout({
                   t.src=v;s=b.getElementsByTagName(e)[0];
                   s.parentNode.insertBefore(t,s)}(window, document,'script',
                   'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${FB_PIXEL_ID}');
+                  fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
                   fbq('track', 'PageView');
                 `,
               }}
